1e080e396a5651fbff68b71bc8283a887b4227ba
atulpandeyy <atulpandit2047@gmail.com>
2026-06-22 16:53:47 +0530

    patterns

--- 01_CPP/01.cpp
+++ 01_CPP/01.cpp
@@ -38,4 +38,28 @@ int main()
     // int max = (x>y) ? x : y;
     // cout<<"max is "<<max;
 
+    // int i=0;
+    // while(i<5)
+    // {
+    //     cout<<i<<endl;
+    //     i++;
+    // }
+
+    //hollow rectangle
+    int n=5;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(i==0||i==4 ||j==0||j==4)
+            {
+                cout<<" *";
+        
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
 }
