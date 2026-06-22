d9f2f493b990d2ebe28ac1190a0bde60d6fe4f85
atulpandeyy <atulpandit2047@gmail.com>
2026-06-22 16:34:27 +0530

    basics

---
+++ 01_CPP/01.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int marks;
+    // int leaves;
+    // cin>>marks>>leaves;
+    // if(marks>50 && leaves>10)
+    // {
+    //     cout<<"pass";
+    // }
+    // else
+    // {
+    //     cout<<"fail";
+    // }
+
+    //switch 
+    // char grade;
+    // cin>>grade;
+    // switch(grade)
+    // {
+    //     case 'A':
+    //         cout<<"Excellent"<<endl;
+    //         break;
+    //     case 'B':
+    //         cout<<"Good"<<endl;
+    //         break;          
+    //     case 'C':
+    //         cout<<"Average"<<endl;
+    //         break;
+    //     default:
+    //         cout<<"Invalid grade";
+    // }
+
+    //ternary operator
+    // int x,y;
+    // cin>>x>>y;
+    // int max = (x>y) ? x : y;
+    // cout<<"max is "<<max;
+
+}
